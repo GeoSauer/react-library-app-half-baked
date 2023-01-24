6b473bd76d543badbd79f58b0a22a5268545e82a
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-23 18:21:30 -0700

    add link to detail page in book titles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import BookList from './components/book/BookList';
 import BookDetail from './components/book/BookDetail';
 import './App.css';
 
 function App() {
-  // TODO: Add routes to books & views
   return (
     <main className="container">
       <Switch>
+        {/* <Route exact path="/">
+          <Link to={`/books`}>
+            <h1>Library Catalog</h1>
+          </Link>
+        </Route> */}
         <Route exact path="/books" component={BookList} />
         <Route path="/books/:id" component={BookDetail} />
       </Switch>
