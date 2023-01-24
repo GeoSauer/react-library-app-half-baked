4ebe67916a6e550a05d588617b61de6bed11f3e0
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-23 18:49:39 -0700

    changed .env.development.local to just .env and now test passing

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-it.skip('renders a welcome message when signed out', async () => {
+it('renders a welcome message when signed out', async () => {
   const { container } = render(
     <MemoryRouter>
       <App />
